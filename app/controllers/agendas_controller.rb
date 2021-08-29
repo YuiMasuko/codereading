c7bf858b66fe4105825f7dbd1cd0c89b6530ba7c
yui masuko <yui.t.tom@gmail.com>
2021-08-29 13:07:29 +0900

    user削除・team編集制限

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -26,7 +26,6 @@ class AgendasController < ApplicationController
     if current_user = @agenda.user || current_user == @agenda.team.owner
       @agenda.destroy
       redirect_to dashboard_url, notice: I18n.t('views.messages.delete_agenda')
-      binding.pry
       @users = @agenda.team.members
       @users.each do |user|
         AgendaMailer.agenda_mail(user.email).deliver
